bf88e4589fcfa377a1e1469132a2680ea9adea68
FasihaNaveed <fasihanaveed777@gmail.com>
2025-07-13 00:16:37 +0500

    comma issue resolve

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch('${API_BASE_URL}/api/listing/get?type=rent&limit=6',
+        const res = await fetch(`${API_BASE_URL}/api/listing/get?type=rent&limit=6`,
         { credentials: "include" });
         const data = await res.json();
         setRentListings(data);
@@ -39,7 +39,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=6',
+        const res = await fetch(`${API_BASE_URL}/api/listing/get?type=sale&limit=6`,
         { credentials: "include" });
         const data = await res.json();
         setSaleListings(data);
